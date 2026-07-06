1abf07209d3df48add66bd83e7d22f54075a8490
atm55555555 <m-mori@lieb.co.jp>
2026-07-06 14:25:33 +0900

    Update URL for reservation page

--- check.py
+++ check.py
@@ -1,7 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 
-URL = "https://hottarakashi-camping.com/reserve/?ym=202607"
+URL = "https://www3.yadosys.com/reserve2/ja/room/calendar/147/ehejfcebejdheigbgihfgpdn/all"
 
 r = requests.get(URL, timeout=30)
 print(r.status_code)
